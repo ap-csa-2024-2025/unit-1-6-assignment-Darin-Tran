1b8f8b6bb4364e3c58e7af395ffbd5f50206d349
Darin-Tran <dhtran122@student.hbuhsd.edu>
2024-09-12 11:21:29 -0700

    finally

--- Problem2.java
+++ Problem2.java
@@ -1,1 +1,14 @@
+public class Problem2
+{
+  public static void main(String[] args)
+  {
+ double num1 = 54.2;
+ double num2 = 102.34;
+
+    int round1 = (int)(num1 + 0.5);
+    int round2 = (int)(num2 + 0.5);
+    int sum = round1 + round2;
+    System.out.print(sum);
+  }
+}
 
--- Problem3.java
+++ Problem3.java
@@ -1,1 +1,17 @@
 
+public class Problem3
+{
+  public static void main(String[] args)
+  {
+ double decimalnum = 50.12421433442;
+
+ double temp = (decimalnum*1000);
+ int num1 = (int)(temp % 10);
+ double temp2 = (temp/10);
+ int num2 = (int)(temp2 % 10);
+ double temp3 = (temp2/10);
+ int num3 = (int)(temp3 % 10);
+ System.out.print(num3+(" ")+num2+(" ")+num1);
+  }
+}
+
